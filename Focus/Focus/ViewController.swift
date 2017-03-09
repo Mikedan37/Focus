afb184f2663853dcaf8c8caa09dee1853d834920
Mikedan37 <M_Danylchuk@Michaels-MacBook.local>
2017-03-08 22:38:30 -0800

    Added a little animation, just playing around but its good to save!

--- Focus/Focus/ViewController.swift
+++ Focus/Focus/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var longhand = UIView()
+    var shorthand = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-    }
+       
+        longhand.frame = CGRect(x: 200 , y: 200 , width: 4 , height: 100 )
+        longhand.backgroundColor = UIColor.red
+        UIView.animate(withDuration: 3, delay: 1, options: [UIViewAnimationOptions.curveLinear, UIViewAnimationOptions.allowUserInteraction,UIViewAnimationOptions.autoreverse,UIViewAnimationOptions.transitionCurlDown], animations: {
+            self.longhand.backgroundColor = UIColor.red
+            self.longhand.frame = CGRect(x: 200, y: 400 , width: 50, height: 150)
+            self.view.addSubview(self.longhand)
+
+        }, completion: nil)
+        view.backgroundColor = UIColor.gray
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
